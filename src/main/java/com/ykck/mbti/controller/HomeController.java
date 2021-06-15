06e96c49473496b3b0ad0de13aab725a59257385
zzase <sehoon9867@naver.com>
2021-06-15 16:18:41 +0900

    sh : homeController 복구

--- src/main/java/com/ykck/mbti/controller/HomeController.java
+++ src/main/java/com/ykck/mbti/controller/HomeController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.ykck.mbti.controller;
 
 import org.springframework.stereotype.Controller;
@@ -34,40 +34,4 @@ public class HomeController {
     }
     
 }
-=======
-package com.ykck.mbti.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/mbti")
-public class HomeController {
-
-    @GetMapping("/home")
-    public String home(){
-        
-        return "index";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-
-        return "member/login";
-    }
-
-    @GetMapping("/cart")
-    public String cart(){
-
-        return "cart/cart";
-    }
-
-    @GetMapping("/survey")
-    public String survey(){
-
-        return "survey";
-    }
-    
-}
->>>>>>> 5e120bf5b4b11935d49fc4b209c21c03db2bca57

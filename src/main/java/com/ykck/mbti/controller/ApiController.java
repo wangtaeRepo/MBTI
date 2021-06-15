002ea38842d0d2fbc732f2978152d70c44a8023a
zzase <sehoon9867@naver.com>
2021-06-15 15:31:53 +0900

    sh:test

--- src/main/java/com/ykck/mbti/controller/ApiController.java
+++ src/main/java/com/ykck/mbti/controller/ApiController.java
@@ -10,7 +10,6 @@ import com.ykck.mbti.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

b0ea66b7df88f82014993beadad1aec45d20134d
minssong1 <rkdalsthd32@gmail.com>
2021-06-15 20:20:11 +0900

    ms : Product Controller, Dto, Service 추가

---
+++ src/main/java/com/ykck/mbti/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.ykck.mbti.service;
+
+import java.util.List;
+
+import com.ykck.mbti.dao.ProductDao;
+import com.ykck.mbti.dto.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductDao productDao;
+
+    public List<Product> getProductsList() {
+        return productDao.getProductsList();
+    }
+
+    public void insertProduct(Product product) {
+        productDao.insertProduct(product);
+    }
+}

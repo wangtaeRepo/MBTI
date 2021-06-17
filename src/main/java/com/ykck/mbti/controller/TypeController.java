9a35c4482fba4893fb63d5a0cf71a1883aa03212
zzase <sehoon9867@naver.com>
2021-06-17 23:53:40 +0900

    sh : 설문조사 후 타입 배정 기능 구현

--- src/main/java/com/ykck/mbti/controller/TypeController.java
+++ src/main/java/com/ykck/mbti/controller/TypeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/mbti")
@@ -17,9 +18,12 @@ public class TypeController {
     TypeService typeService;
 
     @GetMapping("/type-result")
-    public String getTypeById(int type_id,Model model){
-        Type type = typeService.getTypeById(type_id);
+    public String getTypeByCount(@RequestParam("first_category_id") int first_category_id,@RequestParam("second_category_id") int second_category_id,@RequestParam("third_category_id") int third_category_id, Model model){
+
+        Type type = typeService.getTypeByCount(first_category_id,second_category_id,third_category_id);
+        System.out.println(type.getType_name());
         model.addAttribute("type", type);
+
         return "type/type-result";
     }
 }
--- src/main/java/com/ykck/mbti/dao/TypeDao.java
+++ src/main/java/com/ykck/mbti/dao/TypeDao.java
@@ -11,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface TypeDao {
 
     public Type getTypeById(@Param("type_id")int type_id);
+
+    public Type getTypeByCount(@Param("first_category_id") int first_category_id, @Param("second_category_id") int second_category_id, @Param("third_category_id") int third_category_id);
 }
